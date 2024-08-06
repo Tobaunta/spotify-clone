fdc1e086c08bba0eb16ab12d62c890260af848f0
Pixel Pirates <tobauntat@gmail.com>
2024-08-06 14:58:38 +0200

    SideNav added

---
+++ src/components/SideNav/SideNav.jsx
@@ -0,0 +1,8 @@
+import { useState, useEffect } from 'react';
+import { Box, Divider } from '@mui/material';
+
+export const SideNav = ({spotifyApi, token}) => { 
+    return <Box>
+        SideNav
+    </Box>
+ }
