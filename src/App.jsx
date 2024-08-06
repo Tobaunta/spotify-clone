8a6ee40e4e9d761c98275d2c39df870982cddd26
Pixel Pirates <tobauntat@gmail.com>
2024-08-06 14:47:53 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,32 @@ import './App.css';
 import { Box } from '@mui/material';
 import { Dashboard } from './components/Dashboard/Dashboard';
 import { Login } from './pages/Login';
+import { getAccesToken } from './utils/getAccessToken';
+import { getAccessTokenFromStorage } from './utils/getAccessTokenFromStorage';
+import { useEffect, useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 export default function App({ spotifyApi }) {
+	const [token, setToken] = useState(getAccessTokenFromStorage());
+
+	useEffect(() => {
+		const accessToken = getAccessTokenFromStorage() || getAccesToken();
+		if (accessToken) {
+			setToken(accessToken);
+			sessionStorage.setItem('spotifyToken', accessToken);
+			window.location.hash = '';
+		}
+	}, []);
+
 	return (
 		<Box className="App">
-			<Login />
-			<Dashboard spotifyApi={spotifyApi} />
+			{token ? (
+				<Dashboard spotifyApi={spotifyApi} />
+			) : (
+				<Routes>
+					<Route path="*" element={<Login />} />
+				</Routes>
+			)}
 		</Box>
 	);
 }
