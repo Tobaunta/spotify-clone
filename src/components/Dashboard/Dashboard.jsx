626da42613a85b9fe558a4680ca482fef59b140f
Pixel Pirates <tobauntat@gmail.com>
2024-08-06 15:32:29 +0200

    SideNav playlist useEffect added

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -2,12 +2,24 @@ import { Box } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
 import { Home } from '../../pages/Home';
 import { SideNav } from '../SideNav/SideNav';
+import { getAccessTokenFromStorage } from '../../utils/getAccessTokenFromStorage';
+import { useEffect, useState } from 'react';
 
 export const Dashboard = ({ spotifyApi }) => {
+	const [token, setToken] = useState(getAccessTokenFromStorage());
+
+	useEffect(() => {
+		async function onMount() {
+			await spotifyApi.setAccessToken(token);
+		}
+		if (token) {
+			onMount();
+		}
+	}, []);
 	return (
 		<Box sx={{ width: '100%', height: '100%', display: 'flex', flexDirection: 'column' }}>
 			<Box sx={{ flex: 1, overflowY: 'auto', display: 'flex' }}>
-                <SideNav spotifyApi={spotifyApi} token={null} />
+				<SideNav spotifyApi={spotifyApi} token={token} />
 				<Routes>
 					<Route path="/playlist/:id" element={<div>Playlist</div>} />
 					<Route path="/library" element={<div>Library</div>} />
